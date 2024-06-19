280f0e0e893b6c1b19c1ded6ac85971908f76e3f
Jill <jho3292@gmail.com>
2024-06-19 16:29:01 -0400

    Add navigation.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router } from "react-router-dom";
 
+import AddTutorial from "./components/add-tutorial.component";
+import Tutorial from "./components/tutorial.component";
+import TutorialsList from "./components/tutorials-list.component";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        ...
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/tutorials"} className="navbar-brand">
+            bezKoder
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
       </Router>
     );
   }
