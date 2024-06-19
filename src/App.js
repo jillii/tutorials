b11aefe1e618143d56988f99a3e131ae84c146a7
Jill <jho3292@gmail.com>
2024-06-19 16:16:50 -0400

    Add router.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 class App extends Component {
   render() {
-    return;
+    return (
+      <Router>
+        ...
+      </Router>
+    );
   }
 }
 
